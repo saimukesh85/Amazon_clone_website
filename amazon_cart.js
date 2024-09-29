8eeb181b237a7a8a4739fae88c0fb2552d24446d
saimukesh85 <saimukesh85@gmail.com>
2024-09-29 21:42:52 +0530

    update

---
+++ amazon_cart.js
@@ -0,0 +1,28 @@
+function opencart(){
+    window.open("file:///C:/Users/sai%20mukesh/OneDrive/Desktop/programs/projects/amazon-clone/amazon-cart.html");
+    
+}
+let cart_product=``
+/*document.querySelector('.cart-div').addEventListener('click',()=>{
+    cart_items.forEach(item=>{
+        cart_product +=`<div class="maincart">
+        <div class="img1">
+       
+            <img class="cart-img" src="photos/d1.jpg">
+        </div>
+        <div class="pro-det">
+            <div class="pro-name">iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge
+
+        </div>
+        <div class="stock-button">In stock</div>
+        <div class="elig-button">Eligible for FREE Shipping</div>
+        <div class="price-text">
+            <div style="font-weight: bolder; font-size: 20px;">Price:</div>
+            <div style="font-size: 17px;margin-left: 7px; font-weight: bold;color: #b12704; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">&#8377;49,999</div>
+        </div>
+        <div class="quen-button-div"><button class="qty-button">Qty: 1</button></div>
+    </div>
+    </div>`
+    })
+})*/
+
--- amazon_clone.js
+++ amazon_clone.js
@@ -1,40 +1,40 @@
-const pro_details=[
-    img8={id:"img8",
+const pro_details1=[
+    {id:"img8",
     img:"photos/d8.jpg",
     name:"Apple iPhone 13 (128GB) - Starlight",
     price: 41999
     },
-    img9={
+    {
         id:"img9",
     img:"photos/d9.jpg",
     name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
     price: 41999
     },
-    img10={
+    {
         id:"img10",
     img:"photos/d10.jpg",
     name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
     price: 41999
     },
-    img11={
+    {
         id:"img11",
     img:"photos/d11.jpg",
     name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
     price: 41999
     },
-    img12={
+    {
         id:"img12",
     img:"photos/d12.jpg",
     name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
     price: 41999
     },
-    img13={
+    {
         id:"img13",
     img:"photos/d13.jpg",
     name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
     price: 41999
     },
-    img14={
+    {
         id:"img14",
     img:"photos/d14.jpg",
     name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
@@ -43,6 +43,51 @@ const pro_details=[
     
 
 ]
+const pro_details2={
+    img1:{id:"img1",
+    img:"photos/d1.jpg",
+    name:"Apple iPhone 13 (128GB) - Starlight",
+    price: 41999
+    },
+    img2:{
+        id:"img2",
+    img:"photos/d2.jpg",
+    name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
+    price: 41999
+    },
+    img3:{
+        id:"img3",
+    img:"photos/d3.jpg",
+    name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
+    price: 41999
+    },
+    img4:{
+        id:"img4",
+    img:"photos/d4.jpg",
+    name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
+    price: 41999
+    },
+    img5:{
+        id:"img5",
+    img:"photos/d5.jpg",
+    name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
+    price: 41999
+    },
+    img6:{
+        id:"img6",
+    img:"photos/d6.jpg",
+    name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
+    price: 41999
+    },
+    img7:{
+        id:"img7",
+    img:"photos/d7.jpg",
+    name:"iQOO Z9x 5G (Tornado Green, 6GB RAM, 128GB Storage) | Snapdragon 6 Gen 1 with 560k+ AnTuTu Score | 6000mAh Battery with 7.99mm Slim Design | 44W FlashCharge",
+    price: 41999
+    }
+    
+
+}
 function printsearch(){
     console.log(document.querySelector(".searchbar").value);
     document.querySelector(".searchbar").value="";
@@ -65,16 +110,16 @@ function signed(){
         count++;
     }
 }
-let randompro=[8,9,10,11,12,13,14];
+
 let firsthtml="";
-randompro.forEach((item)=>{
-    firsthtml+=`<div><img class="im" data-product="img${item}.id"src="photos/d${item}.jpg"><button class="add-to-cart-button" onclick="addtocart();">Add to Cart</button></div>`
+Object.values(pro_details1).forEach((item)=>{
+    firsthtml+=`<div><img class="im" src="${item.img}"><button class="add-to-cart-button" data-product-id="${item.id}" onclick="addtocart();">Add to Cart</button></div>`
 })
 document.querySelector(".products").innerHTML=firsthtml;
-let decarativeproduct=[1,2,3,4,5,6,7]
+
 let secondhtml="";
-decarativeproduct.forEach((item)=>{
-    secondhtml +=`<div><button class="second-row-product-img"><img src="photos/d${item}.jpg"></button><button class="add-to-cart-button" onclick="addtocart();">Add to Cart</button></div>`
+Object.values(pro_details2).forEach((item)=>{
+    secondhtml +=`<div><button class="second-row-product-img"><img src="${item.img}"></button><button class="add-to-cart-button" data-product-id="${item.id}" onclick="addtocart();">Add to Cart</button></div>`
 }
 )
 document.querySelector('.second-row-of-items').innerHTML=secondhtml;
@@ -84,11 +129,37 @@ function addtocart(){
     document.querySelector(".cart-count-num").innerHTML=cart_count;
     cart_count++;
 };
+let cart_items=[];
+const addcart=document.querySelectorAll('.add-to-cart-button');
+addcart.forEach(button=>{
+    button.addEventListener('click',()=>{
+        let link=button.dataset.productId;
+        Object.values(pro_details1).forEach(pro=>{
+            if(pro.id === link){
+                cart_items.push(pro)
+            }
+        })
+        
+    })
+    
+})
+addcart.forEach(button=>{
+    button.addEventListener('click',()=>{
+        let link=button.dataset.productId;
+        Object.values(pro_details2).forEach(pro=>{
+            if(pro.id === link){
+                cart_items.push(pro)
+            }
+        })
+    })
+    
+    
+})
+
 /* --------------code for second page--------------------*/
 
-function opencart(){
-    window.open("file:///C:/Users/sai%20mukesh/OneDrive/Desktop/programs/projects/amazon-clone/amazon-cart.html");
-}
+    
+    
 
     
 
